b481b5be9ccb0acd949aff692be957e2fbb4f2c7
Rishabh Pandey <rishabh.pandey995@gmail.com>
2025-02-18 18:32:23 +0530

    misc updates

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -7,9 +7,13 @@ description: Get in touch
 @section('body')
     <h1>Contact</h1>
 
-    {{--<p class="mb-8">--}}
-    {{--    You can find me on twitter.--}}
-    {{--</p>--}}
+    <p class="mb-8">
+        I am a web developer specializing in building and designing exceptional digital experiences. If you have any
+        questions, projects, or just want to say hi, feel free to reach out using the form below.
+    </p>
+    {{-- <p class="mb-8"> --}}
+    {{--    You can find me on twitter. --}}
+    {{-- </p> --}}
 
     <form action="/message-sent" method="post" id="contact-form" class="mb-12" data-netlify="true">
         <div class="flex flex-wrap mb-6 -mx-3">
@@ -18,14 +22,8 @@ description: Get in touch
                     Name
                 </label>
 
-                <input
-                        type="text"
-                        id="contact-name"
-                        placeholder="Jane Doe"
-                        name="name"
-                        class="block w-full border shadow rounded-lg outline-none mb-2 px-4 py-3"
-                        required
-                >
+                <input type="text" id="contact-name" placeholder="Jane Doe" name="name"
+                    class="block w-full border shadow rounded-lg outline-none mb-2 px-4 py-3" required>
             </div>
 
             <div class="w-full px-3 md:w-1/2">
@@ -33,14 +31,8 @@ description: Get in touch
                     Email Address
                 </label>
 
-                <input
-                        type="email"
-                        id="contact-email"
-                        placeholder="email@domain.com"
-                        name="email"
-                        class="block w-full border shadow rounded-lg outline-none mb-2 px-4 py-3"
-                        required
-                >
+                <input type="email" id="contact-email" placeholder="email@domain.com" name="email"
+                    class="block w-full border shadow rounded-lg outline-none mb-2 px-4 py-3" required>
             </div>
         </div>
 
@@ -49,22 +41,14 @@ description: Get in touch
                 Message
             </label>
 
-            <textarea
-                    id="contact-message"
-                    rows="4"
-                    name="message"
-                    class="block w-full border shadow rounded-lg outline-none appearance-none mb-2 px-4 py-3"
-                    placeholder="A lovely message here."
-                    required
-            ></textarea>
+            <textarea id="contact-message" rows="4" name="message"
+                class="block w-full border shadow rounded-lg outline-none appearance-none mb-2 px-4 py-3"
+                placeholder="A lovely message here." required></textarea>
         </div>
 
         <div class="flex justify-end w-full">
-            <input
-                    type="submit"
-                    value="Submit"
-                    class="block bg-blue-500 hover:bg-blue-600 text-white text-sm font-semibold leading-snug tracking-wide uppercase shadow rounded-lg cursor-pointer px-6 py-3"
-            >
+            <input type="submit" value="Submit"
+                class="block bg-blue-500 hover:bg-blue-600 text-white text-sm font-semibold leading-snug tracking-wide uppercase shadow rounded-lg cursor-pointer px-6 py-3">
         </div>
     </form>
 @stop
